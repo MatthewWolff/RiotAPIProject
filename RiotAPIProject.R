5952ddec3e8b147f35fbc04c115730da65f37f6e
Matthew Wolff <mm.wolff@chi-squared.org>
2018-03-12 00:54:57 -0500

    changed = to <- as per R standard

--- RiotAPIProject.R
+++ RiotAPIProject.R
@@ -20,14 +20,14 @@ library(fields)
 
 # functions for converting list to a data.frame, where var is the indices,
 # and myList is the list that they're applied to
-grabInfo<-function(var, myList)
+grabInfo <- function(var, myList)
 {
   sapply(myList, function(x) returnData(x, var)) 
 }
 
 # checks if data is there. if not, supplies a null. Carries on from grabInfo(). 
 # Var = indices
-returnData<-function(x, var)
+returnData <- function(x, var)
 {
   if(!is.null( x[[var]]))
   {
@@ -39,7 +39,7 @@ returnData<-function(x, var)
 
 ############################## functions that access API ###############################
 #grabs a random summoner from list
-randomSummoner = function()
+randomSummoner <- function()
 {
   found <- FALSE #sentinel value
   while(!found)
@@ -53,7 +53,7 @@ randomSummoner = function()
 }
 
 # the game stores characters as numbers, but players use names. Retrieves name when given number
-champion = function(championId)
+champion <- function(championId)
 {
   if(!is.numeric(championId)) #must be numeric
     stop("ChampionId is not a numeric")
@@ -67,7 +67,7 @@ champion = function(championId)
 }
 
 # retrieves a player's data from their summoner name
-summoner = function(summonerName)
+summoner <- function(summonerName)
 {
   summonerName = gsub(" ", "", summonerName, fixed = TRUE) #remove whitespace
   URL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"
@@ -81,7 +81,7 @@ summoner = function(summonerName)
 }
 
 #retrieves a summoner's 10 most recent games from their summoner name, utilizes summoner()
-summonerRecent = function(summonerName)
+summonerRecent <- function(summonerName)
 {
   URL = "https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/"
   api_key = "/recent?api_key=826630f6-9e7d-4cfe-802f-9c2614832d97"
@@ -91,7 +91,7 @@ summonerRecent = function(summonerName)
 }
 
 #retrieves ranked game statistics for a summoner (ranked games contribute to your standing)
-summonerRecentRanked = function(summonerName)
+summonerRecentRanked <- function(summonerName)
 {
   URL = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/"
   api_key = "/ranked?season=SEASON2016&api_key=826630f6-9e7d-4cfe-802f-9c2614832d97"
@@ -101,7 +101,7 @@ summonerRecentRanked = function(summonerName)
 }
 
 # shows the frequency of characters played from last 10 games, utilizes summonerRecent()
-recentPlay = function(summonerName) #plots champions from last 10 games
+recentPlay <- function(summonerName) #plots champions from last 10 games
 {
   recent = summonerRecent(summonerName); #will generate warnings
   champions = unlist(
@@ -110,7 +110,7 @@ recentPlay = function(summonerName) #plots champions from last 10 games
   barplot(table(champions),ylab = "# of Recent Games Played", xlab = "", las = 3, col = "blue", cex.axis = 1,
           main = paste(summonerName,"'s Champions in Recent Matches"))
 }
-recentWin = function(summonerName)
+recentWin <- function(summonerName)
 {
   recentGames <- summonerRecent(summonerName)
   barplot(table(unlist(
@@ -119,17 +119,17 @@ recentWin = function(summonerName)
     main = paste(summonerName,"'s Recent Matches", sep = ""), ylab = "Games", col = c("red3", "green3"), horiz = TRUE)
 }
 #analyze all recent matches of a given summoner
-analyzeRecent = function(summonerName)
+analyzeRecent <- function(summonerName)
 {
   par(mfrow=c(3,1))
   recentPlay(summonerName) #will throw warning
   recentWin(summonerName)
-  recentGames = summonerRecent(summonerName) #grabs their 10 most recent games
-  pulledData = lapply(recentGames$games[seq_along(recentGames$games)],
+  recentGames <- summonerRecent(summonerName) #grabs their 10 most recent games
+  pulledData <- lapply(recentGames$games[seq_along(recentGames$games)],
                       function(x) x$stats) #extracts the stats
-  gamesWon = which(unlist(
+  gamesWon <- which(unlist(
     lapply(pulledData[seq_along(pulledData)],
-           function(x) x$win))== TRUE) #checks which games the player won
+           function(x) x$win)) == TRUE) #checks which games the player won
   barplot(table(champion(unlist(lapply(recentGames$games[gamesWon], function(x) x$championId)))),
           main = paste(summonerName, "'s Winning Champions in Last 10 Games"),
           ylab = "# Games Won", xlab = "Champion", las = 3, col = "cyan4")
@@ -142,9 +142,9 @@ riotSampleAPI <- fromJSON("/users/matthew/downloads/matches1.json")
 championList <- data.frame(sapply(1:4,
                                   function(x) grabInfo(x, champList$data)), stringsAsFactors = FALSE)
 colnames(championList) <- unlist(attributes(champList$data$Thresh))
-summonerList = c() #list to fill
+summonerList <- c() #list to fill
 for(i in 1:100) #grabs players from all 100 games
-  summonerList = c(summonerList, unlist(
+  summonerList <- c(summonerList, unlist(
     lapply(riotSampleAPI$matches[[i]]$participantIdentities[1:10],
            function(x) x$player$summonerName)))
 
@@ -155,7 +155,7 @@ for(i in 1:100) #grabs players from all 100 games
 #match lengths and median match length
 matches <- riotSampleAPI[[1]]
 cat(c("Number of Matches:", length(matches))) #number of games in data set
-matchTimes = round(unlist(
+matchTimes <- round(unlist(
   lapply(matches[seq_along(matches)],
          function(x) x$matchDuration/60))) #rounds games out to the minute
 plot(table(matchTimes), xlab = "Game Duration (min)", ylab = "% of Games ",
@@ -174,7 +174,7 @@ unlist(lapply(rankedGames$champions[seq_along(rankedGames$champions)],
 ###### NOTE: look at output for champion names
 # Looks at 10 most recent games and creates a barplot of champion frequency,
 # then looks at how many of those you won. 
-randSummoner = randomSummoner()
+randSummoner <- randomSummoner()
 matches2 <- summonerRecent(randSummoner)
 matchTimes <- round(unlist(lapply(matches2$games[seq_along(matches2$games)],
                                   function(x) x$stats$timePlayed/60)))
@@ -183,17 +183,17 @@ plot(table(matchTimes),
 xline(median(matchTimes), lwd = 5, col = "red") #average match length
 legend("topright", "Median Match Length", pch = 15, inset = .05, col = "red")
 
-par(mfrow=c(3,1)); #so I can show all my plots
+par(mfrow = c(3,1)); #so I can show all my plots
 recentPlay(randSummoner) #function I created
 recentWin(randSummoner)  #function I created
 
 #This looks at your last 10 games and determines who you won most as
-recentGames = summonerRecent(randSummoner) #grabs their 10 most recent games
-pulledData = lapply(recentGames$games[seq_along(recentGames$games)],
+recentGames <- summonerRecent(randSummoner) #grabs their 10 most recent games
+pulledData <- lapply(recentGames$games[seq_along(recentGames$games)],
                     function(x) x$stats) #extracts the stats
-gamesWon = which(unlist(
+gamesWon <- which(unlist(
   lapply(pulledData[seq_along(pulledData)],
-         function(x) x$win))== TRUE) #checks which games the player won
+         function(x) x$win)) == TRUE) #checks which games the player won
 barplot(table(champion(unlist(lapply(recentGames$games[gamesWon], function(x) x$championId)))),
         main = paste(randSummoner, "'s Winning Champions in Last 10 Games", sep = ""),
         ylab = "# Games Won", xlab = "Champion", las = 3, col = "cyan4")
